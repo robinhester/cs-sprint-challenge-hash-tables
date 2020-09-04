099b90c29cbdd448aea4243eb299de20ded2ab9d
Robin Hester <robin.hester@yahoo.com>
2020-09-04 14:00:11 -0400

    i of 2 passed ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,21 +20,51 @@ def reconstruct_trip(tickets, length):
         maps.append(routes)
 
     print(maps)
+    print(len(maps))
+    print(maps[1]['source'])
 
-    # route.append(maps["None"])
+    maps_length = len(maps) - 1
 
-    # tic_length = len(tickets)
+    for i in range(0, maps_length):
 
-    # for i in range(0, tic_length):
-    #     route.append(maps[route[i-1]])
+        if maps[i]['source'] == 'NONE':
+            route.insert(0, maps[i]['destination'])
+        if len(route) == 1:
+            if maps[i]['source'] == route[0]:
+                route.append(maps[i]['destination'])
+        if len(route) == 2:
+            if maps[i]['source'] == route[1]:
+                route.append(maps[i]['destination'])
+        if len(route) >2:
+            if maps[i]['source'] == route[i-1]:
+                route.append(maps[i]['destination'])
+        
+        # if maps[i]['source'] == 'None':
+        #     route.append(maps[i]['destination'])
+        # if maps[i[['source'] == maps[i+1]['destination']:
+        #     route.append(maps[i]['destination'])
+        # if maps[i]['destination'] == 'None':
+        #     route.append(maps[i]['source'])
 
+
+    
+    route.append('NONE')
+    print(route)
     return route
 
-ticket_1 = Ticket("NONE", "PDX")
-ticket_2 = Ticket("PDX", "DCA")
-ticket_3 = Ticket("DCA", "NONE")
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
 
-tickets = [ticket_1, ticket_2, ticket_3]
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+                   ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
 
-reconstruct_trip(tickets, 3)
+reconstruct_trip(tickets, 10)
 
